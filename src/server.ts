fdff34fea201907ef119d84a85983b3a4662f2ce
saifulislammunna <saifulmunna28@gmail.com>
2026-05-16 20:23:53 +0600

    express-learning

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import express, { type Application, type Request, type Response } from "express"
+ 
+const app : Application = express()
+const port = 5000;
+
+app.use(express.json())
+
+app.get('/', (req : Request, res : Response) => {
+//   res.send('Hello World!!!')
+   res.status(200).json({
+    "message" : "Express Server",
+    "author" : "Next level",
+    })
+});
+
+app.post('/', async(req : Request,res : Response) => {
+    console.log(req.body);
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
