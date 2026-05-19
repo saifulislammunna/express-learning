8c4049e15098d50a48abeca9d6b51df16b23661a
saifulislammunna <saifulmunna28@gmail.com>
2026-05-19 16:38:55 +0600

    server-added

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,38 @@
 import express, { type Application, type Request, type Response } from "express"
- 
+import {Pool} from "pg" 
+
 const app : Application = express()
 const port = 5000;
 
-app.use(express.json())
+app.use(express.json());
+app.use(express.text());
+app.use(express.urlencoded({extended : true}));
+
+const pool = new Pool({
+  connectionString : "postgresql://neondb_owner:npg_2r6tezRWCnOa@ep-frosty-salad-aqv942li-pooler.c-8.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+})
+
+const initDB = async () => {
+  try {
+    await pool.query(`
+        CREATE TABLE IF NOT EXISTS users(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(50),
+        email VARCHAR(50) UNIQUE NOT NULL,
+        password VARCHAR(50) NOT NULL,
+        is_active BOOLEAN DEFAULT true,
+        age INT,
+
+        created_at TIMESTAMP DEFAULT NOW(),
+        updated_at TIMESTAMP DEFAULT NOW()
+        )
+            `);
+    console.log("Database connected successfully!");
+  } catch (error) {
+    console.log(error);
+  }
+};
+initDB(); 
 
 app.get('/', (req : Request, res : Response) => {
 //   res.send('Hello World!!!')
@@ -14,8 +43,28 @@ app.get('/', (req : Request, res : Response) => {
 });
 
 app.post('/', async(req : Request,res : Response) => {
-    console.log(req.body);
-})
+    // console.log(req.body);
+    const {name,email,password,age} = req.body;
+    
+try {
+      const result = await pool.query(`
+
+      INSERT INTO users (name,email,password,age) VALUES($1,$2,$3,$4)
+      RETURNING *
+      `,[ name, email,password,age],);
+      //  console.log(result);
+
+    res.status(201).json({
+      message : "User Created successfully",
+      data : result.rows[0],
+    });
+} catch (error : any) {
+   res.status(500).json({
+      message : error.message,
+      error : error,
+    });
+}
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
